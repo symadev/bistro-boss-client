91256cc764e68c0936c92e41f4ae859695a66e6f
syma sultana <symasultana2002@gmail.com>
2025-04-07 16:21:11 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Tailwind will scan all files inside src
+    "./public/index.html"         // Optional: if you're referencing classes in index.html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
