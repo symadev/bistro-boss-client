950ac0a60be6166616ec3c081cf2819140760354
syma sultana <symasultana2002@gmail.com>
2025-04-08 17:41:51 +0600

    added the navbar section

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,52 +1,45 @@
-
+import { FaShoppingCart, FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
-    const links = <>
-    
-    <li><a>Item 1</a></li>
-          
-              <li><a>Item 3</a></li>
+  const links = (
+    <>
+      <li><a className="text-yellow-400 font-bold">HOME</a></li>
+      <li><a>CONTACT US</a></li>
+      <li><a>DASHBOARD</a></li>
+      <li><a>OUR MENU</a></li>
+      <li><a>OUR SHOP</a></li>
     </>
-    return (
-        <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16" />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+  );
 
-             {links}
-
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">BISTRO BOSS</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-
-           {links}
-
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
-        </div>
+  return (
+    <div className="navbar fixed z-10 bg-black bg-opacity-30 text-white max-w-screen-xl px-4">
+      {/* Left: Logo */}
+      <div className="navbar-start">
+        <a className="text-xl font-bold leading-4">
+          BISTRO BOSS
+          <br />
+          <span className="text-sm tracking-[0.25em] font-light">RESTAURANT</span>
+        </a>
       </div>
-    );
+
+      {/* Center: Menu links */}
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-3 text-sm font-semibold">
+          {links}
+        </ul>
+      </div>
+
+      {/* Right: Cart and User */}
+      <div className="navbar-end gap-4">
+        <div className="relative">
+          <FaShoppingCart className="text-xl" />
+          <span className="badge badge-sm badge-secondary absolute -top-2 -right-2">3</span>
+        </div>
+        <a className="text-sm font-semibold">SIGN OUT</a>
+        <FaUserCircle className="text-2xl" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
