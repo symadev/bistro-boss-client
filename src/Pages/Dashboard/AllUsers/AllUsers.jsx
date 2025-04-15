63f6b9ecf1928f0c8d562976048d7aca2c0a511b
syma sultana <symasultana2002@gmail.com>
2025-04-15 22:50:18 +0600

    Display All users and Create Make admin API

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,16 +1,58 @@
 import { useQuery } from "@tanstack/react-query";
 import UseAxiosSecure from "../../../Components/UseAuth/UseAxiosSecure";
+import Swal from "sweetalert2";
+import { FaTrashAlt, FaUser } from "react-icons/fa";
 
 
 const AllUsers = () => {
+
+
+
     const axiosSecure = UseAxiosSecure()
-    const { data, users=[] } = useQuery({
+    const { data :users=[],refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async() =>{
           const res = await axiosSecure.get('/users')
            return  res.data;
         }
       })
+
+      const handleMakeAdmin = user =>{
+
+      }
+
+      const handleDeleteUser = user =>{
+    
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+           
+            
+            axiosSecure.delete(`/users/${user._id}`)
+            .then(res=>{
+                if(res.data.deletedCount > 0){
+                    refetch()
+            Swal.fire({
+                
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+
+                }
+                
+            })
+        }
+          });
+
+    }
     return (
         <div >
             <div className="flex justify-evenly mb-8">
@@ -28,18 +70,23 @@ const AllUsers = () => {
       <tr>
         <th></th>
         <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>Email</th>
+        <th>Users</th>
+        <th>Action</th>
       </tr>
     </thead>
     <tbody>
       {/* row 1 */}
       {
-        users.map(user=><tr  key={user._id}>
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+        users.map((user,index)=><tr  key={user._id}>
+            <th>{index+1}</th>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td><button onClick={()=> handleMakeAdmin(user)} className="btn btn-primary btn-lg">
+            <FaUser className="text-red-500"></FaUser></button></td>
+
+            <td> <button onClick={()=> handleDeleteUser(user)} className="btn btn-ghost btn-lg">
+                <FaTrashAlt className="text-red-500"></FaTrashAlt></button></td>
           </tr>)
       }
       
