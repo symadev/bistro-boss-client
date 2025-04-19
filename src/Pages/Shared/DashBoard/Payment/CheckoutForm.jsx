469bcbe3487db824fdf285a2457da670406c0cee
syma sultana <symasultana2002@gmail.com>
2025-04-19 20:33:41 +0600

    delete cart items in the database

--- src/Pages/Shared/DashBoard/Payment/CheckoutForm.jsx
+++ src/Pages/Shared/DashBoard/Payment/CheckoutForm.jsx
@@ -2,38 +2,41 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import UseAxiosSecure from "../../../../Components/UseAuth/UseAxiosSecure";
 import UseCart from "../../../../Components/UseAuth/UseCart";
+import UseAuth from "../../../../Components/UseAuth/UseAuth";
 
 const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
     const [errorMessage, setErrorMessage] = useState('');
-    const [clientSecret,setClientSecret] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const [transactionId, setTransactionId] = useState('')
+    const { user } = UseAuth()
     //user log-in kora thakbe tai amra aikahne axiosSecure use korbo
-const axiosSecure = UseAxiosSecure()
-const [cart ]= UseCart()
-const totalPrice = cart.reduce((total,item )=>total+item.price,0);
+    const axiosSecure = UseAxiosSecure()
+    const [cart] = UseCart()
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
 
 
 
     //now we send the data to the backend 
-    useEffect(() =>{
-        axiosSecure.post('/create-payment-intent',{price: totalPrice})
-        .then (res=>{
-            console.log(res.data.clientSecret)
-            setClientSecret(res.data.clientSecret)
-        })
+    useEffect(() => {
+        axiosSecure.post('/create-payment-intent', { price: totalPrice })
+            .then(res => {
+                console.log(res.data.clientSecret)
+                setClientSecret(res.data.clientSecret)
+            })
 
-    },[axiosSecure,totalPrice])
+    }, [axiosSecure, totalPrice])
 
 
-//     useEffect এর কারণে এই call শুধু component mount হওয়ার পর একবারই হবে।
+    //     useEffect এর কারণে এই call শুধু component mount হওয়ার পর একবারই হবে।
 
-// যখন checkout form শুরু হবে, তখনই Stripe-এর জন্য server-side থেকে clientSecret তৈরি করে আনা হবে,
-//  যেটা Stripe পেমেন্ট authorize করার জন্য দরকার।
+    // যখন checkout form শুরু হবে, তখনই Stripe-এর জন্য server-side থেকে clientSecret তৈরি করে আনা হবে,
+    //  যেটা Stripe পেমেন্ট authorize করার জন্য দরকার।
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        
+
 
         if (!stripe || !elements) {
             // Stripe.js has not loaded yet. Make sure to disable
@@ -55,23 +58,76 @@ const totalPrice = cart.reduce((total,item )=>total+item.price,0);
 
 
         // Use your card Element with other Stripe.js APIs
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card,
-      });
-  
-      if (error) {
-        console.log('[error]', error);
-        setErrorMessage(errorMessage.message)
-      } else {
-        console.log('[PaymentMethod]', paymentMethod);
-        setErrorMessage('');
-      }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+        if (error) {
+            console.log('[error]', error);
+            setErrorMessage(error.message);
+        } else {
+            console.log('[PaymentMethod]', paymentMethod);
+            setErrorMessage('');
+        }
+
+
+
+        //confirm payment
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        email: user?.email || 'anonymous',
+                        name: user?.displayName || 'anonymous',
+                    },
+                },
+            }
+        );
+
+        if (confirmError) {
+            console.log('confirmError', confirmError.message);
+            setErrorMessage(confirmError.message);
+        }
+        else {
+            console.log(' paymentIntent', paymentIntent);
+            if (paymentIntent.status === "succeeded") {
+                console.log('transaction id', paymentIntent.id);
+                setTransactionId(paymentIntent.id)
+
+
+
+
+                //now save the payment to the database
+                //client code
+                const payment = {
+                    email: user.email,
+                    price: totalPrice,
+                    transactionId:paymentIntent.id,
+                    date: new Date(),//utc date convert,use moment js to 
+                    cartIds: cart.map(item => item._id),
+                    menuItemIds: cart.map(item => item.menuId),
+                    status:"pending",
+                       
+                }
+                const res = await axiosSecure.post('/payments', payment)
+                console.log('payment saved', res.data);
+            }
+
+        }
+
+
+
+
     };
-  
 
 
-   
+
+
+
+
 
     return (
         <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4 bg-white rounded shadow-md space-y-4">
@@ -93,12 +149,15 @@ const totalPrice = cart.reduce((total,item )=>total+item.price,0);
             />
             <button
                 type="submit"
-                disabled={!stripe||! clientSecret}
+                disabled={!stripe || !clientSecret}
                 className="w-full bg-purple-600 text-white py-2 rounded hover:bg-purple-700 transition"
             >
                 Pay
             </button>
             <><p>{errorMessage}</p>
+                {transactionId && <p className="text-green-400"> your transaction id:{transactionId}</p>
+
+                }
             </>
         </form>
     );
